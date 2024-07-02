878d08f290701d3b193e4a7d28cdd7ae049c1446
juniorgala23 <juniorgala23@gmail.com>
2024-07-02 21:05:00 +0000

    Conection to Github from Project IDX - Only Test

---
+++ Hello_World.py
@@ -0,0 +1,17 @@
+"""
+This function defines a simple program that prints "Hello World!" to the console when run. 
+The use of the if __name__ == "__main__": block is a common practice in Python to ensure 
+that certain code is only executed when the script is run directly and the main function is called.
+The aim was to establish a test connection with GitHub.
+"""
+
+# Define the main function
+def main():
+    # Print "Hello World!" to the console
+    print("Hello World!")
+
+# Check if the script is being run directly
+if __name__ == "__main__":
+    # Call the main function
+    main()
+
